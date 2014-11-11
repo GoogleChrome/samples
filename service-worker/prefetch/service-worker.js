75f53013761003796512ed088316b0bcbc0c7114
Jeff Posnick <jeffy@google.com>
2014-11-11 17:12:10 -0500

    Switched to the polyfilled methods.

--- service-worker/prefetch/service-worker.js
+++ service-worker/prefetch/service-worker.js
@@ -1,3 +1,7 @@
+// This polyfill provides Cache.add(), Cache.addAll(), and CacheStorage.match(),
+// which are not implemented in Chrome 40.
+importScripts('../serviceworker-cache-polyfill.js');
+
 self.addEventListener('install', function(event) {
   var urlsToPrefetch = [
     './static/pre_fetched.txt',
@@ -12,40 +16,23 @@ self.addEventListener('install', function(event) {
 
   event.waitUntil(
     caches.open('prefetch-sample').then(function(cache) {
-      // This two-stage fetch() + cache.put() combo is required because Chrome doesn't
-      // currently support cache.addAll(). Once support for that is added, it's preferable
-      // to use that.
-      // TODO: Switch code to use cache.addAll() when available.
-      return Promise.all(
-        urlsToPrefetch.map(function(url) {
-          // It's very important to use { mode: 'no-cors' } if there is any chance that
-          // the resources being fetched are served off of a server that doesn't support
-          // CORS (http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
-          // In this example, www.chromium.org doesn't support CORS, and the fetch()
-          // would fail if the default mode of 'cors' was used for the fetch() request.
-          // The drawback of hardcoding {mode: 'no-cors'} is that the response from all
-          // cross-origin hosts will always be opaque
-          // (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#cross-origin-resources)
-          // but since this sample is focused on caching rather than modifying the responses, that is
-          // not a problem.
-          return fetch(url, {mode: 'no-cors'}).then(function(response) {
-            console.log('Fetching of', url, 'succeeded. Response is', response);
-
-            return response;
-          });
-        })
-      ).then(function(responses) {
-        // Cache each of the responses, using the URL of the request as the key.
-        return responses.map(function(response, i) {
-          return cache.put(urlsToPrefetch[i], response);
-        });
-      }).then(function(responses) {
-        console.log('Caching of fetched responses is complete.');
-
-        return responses;
+      cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+        // It's very important to use { mode: 'no-cors' } if there is any chance that
+        // the resources being fetched are served off of a server that doesn't support
+        // CORS (http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
+        // In this example, www.chromium.org doesn't support CORS, and the fetch()
+        // would fail if the default mode of 'cors' was used for the fetch() request.
+        // The drawback of hardcoding {mode: 'no-cors'} is that the response from all
+        // cross-origin hosts will always be opaque
+        // (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#cross-origin-resources)
+        // and it is not possible to determine whether an opaque response represents a success or failure
+        // (https://github.com/whatwg/fetch/issues/14).
+        return new Request(urlToPrefetch, { mode: 'no-cors' });
+      })).then(function() {
+        console.log('All resources have been fetched and cached.');
       });
     }).catch(function(error) {
-      // This catch() will handle any exceptions from the fetch()/cache.put() steps.
+      // This catch() will handle any exceptions from the caches.open()/cache.addAll() steps.
       console.error('Pre-fetching failed:', error);
     })
   );
@@ -55,29 +42,28 @@ self.addEventListener('fetch', function(event) {
   console.log('Handling fetch event for', event.request.url);
 
   event.respondWith(
-    caches.open('prefetch-sample').then(function(cache) {
-      // TODO: Switch from catch()ing to checking if response is undefined once http://crbug.com/428486 is resolved.
-      return cache.match(event.request).then(function(response) {
+    caches.match(event.request).then(function(response) {
+      if (response) {
         console.log('Found response in cache:', response);
 
         return response;
-      }).catch(function() {
-        console.log('No response found in cache. About to fetch from network...');
+      }
 
-        // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
-        // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
-        return fetch(event.request).then(function(response) {
-          console.log('Response from network is:', response);
+      console.log('No response found in cache. About to fetch from network...');
 
-          return response;
-        }).catch(function(error) {
-          // This catch() will handle exceptions thrown from the fetch() operation.
-          // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-          // It will return a normal response object that has the appropriate error code set.
-          console.error('Fetching failed:', error);
+      // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+      // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+      return fetch(event.request).then(function(response) {
+        console.log('Response from network is:', response);
 
-          throw error;
-        });
+        return response;
+      }).catch(function(error) {
+        // This catch() will handle exceptions thrown from the fetch() operation.
+        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+        // It will return a normal response object that has the appropriate error code set.
+        console.error('Fetching failed:', error);
+
+        throw error;
       });
     })
   );
