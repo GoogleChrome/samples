6733bf55a51f5c1ee16f52bd4b99eb6c981d9527
Jeff Posnick <jeffy@google.com>
2014-11-18 17:14:49 -0500

    Service Worker -> service worker

--- service-worker/registration-events/service-worker.js
+++ service-worker/registration-events/service-worker.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(e) {
   // It's an opportunity to initialize caches and prefetch data, if desired. This sort of
   // work should be wrapped in a Promise, and e.waitUntil(promise) can be used to ensure that
   // this installation does not complete until the Promise is settled.
-  // Also, be aware that there may already be an existing Service Worker controlling the page
+  // Also, be aware that there may already be an existing service worker controlling the page
   // (either an earlier version of this script or a completely different script.)
   console.log('Install event:', e);
 });
@@ -20,8 +20,8 @@ self.addEventListener('activate', function(e) {
   // e.waitUntil(promise) is also available here to delay activation until work has been performed,
   // but note that waiting within the activate event will delay handling of any
   // fetch or message events that are fired in the interim. When possible, do work during the install phase.
-  // It will NOT be fired each time the Service Worker is revived after being terminated.
-  // To perform an action when the Service Worker is revived, include that logic in the
+  // It will NOT be fired each time the service worker is revived after being terminated.
+  // To perform an action when the service worker is revived, include that logic in the
   // `onfetch` or `onmessage` event listeners.
   console.log('Activate event:', e);
 });
