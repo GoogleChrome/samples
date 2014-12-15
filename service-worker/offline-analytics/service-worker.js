854bddaa9855273b3b53f590b8a78deed1147fad
Jeff Posnick <jeffy@google.com>
2014-12-15 12:41:40 -0500

    Additional comments/explanatory text.

--- service-worker/offline-analytics/service-worker.js
+++ service-worker/offline-analytics/service-worker.js
@@ -30,25 +30,32 @@ var CURRENT_CACHES = {
 };
 
 var idbDatabase;
+var IDB_VERSION = 1;
 var STORE_NAME = 'urls';
 
+// This is basic boilerplate for interacting with IndexedDB. Adapted from
+// https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
 function openDatabaseAndReplayRequests() {
-  var indexedDBOpenRequest = indexedDB.open('offline-analytics', CACHE_VERSION);
+  var indexedDBOpenRequest = indexedDB.open('offline-analytics', IDB_VERSION);
 
+  // This top-level error handler will be invoked any time there's an IndexedDB-related error.
   indexedDBOpenRequest.onerror = function(error) {
     console.error('IndexedDB error:', error);
   };
 
+  // This should only execute if there's a need to create a new database for the given IDB_VERSION.
   indexedDBOpenRequest.onupgradeneeded = function() {
     this.result.createObjectStore(STORE_NAME, {keyPath: 'url'});
   };
 
+  // This will execute each time the database is opened.
   indexedDBOpenRequest.onsuccess = function() {
     idbDatabase = this.result;
     replayAnalyticsRequests();
   };
 }
 
+// Helper method to get the object store that we care about.
 function getObjectStore(storeName, mode) {
   return idbDatabase.transaction(storeName, mode).objectStore(storeName);
 }
@@ -57,6 +64,7 @@ function replayAnalyticsRequests() {
   var savedRequests = [];
 
   getObjectStore(STORE_NAME, 'readonly').openCursor().onsuccess = function(event) {
+    // See https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB#Using_a_cursor
     var cursor = event.target.result;
 
     if (cursor) {
@@ -69,15 +77,21 @@ function replayAnalyticsRequests() {
 
       savedRequests.forEach(function(savedRequest) {
         var queueTime = Date.now() - savedRequest.timestamp;
+        // The qt= URL parameter specifies the time delta in between right now, and when the
+        // /collect request was initially intended to be sent. See
+        // https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#qt
         var requestUrl = savedRequest.url + '&qt=' + queueTime;
 
         console.log(' Replaying', requestUrl);
 
         fetch(requestUrl).then(function(response) {
           if (response.status < 400) {
+            // If sending the /collect request was successful, then remove it from the IndexedDB.
             getObjectStore(STORE_NAME, 'readwrite').delete(savedRequest.url);
             console.log(' Replaying succeeded.');
           } else {
+            // This will be triggered if, e.g., Google Analytics returns a HTTP 50x response.
+            // The request will be replayed the next time the service worker starts up.
             console.error(' Replaying failed:', response);
           }
         }).catch(function(error) {
@@ -90,6 +104,10 @@ function replayAnalyticsRequests() {
   };
 }
 
+// Open the IndexedDB and check for requests to replay each time the service worker starts up.
+// Since the service worker is terminated fairly frequently, it should start up again for most
+// page navigations. It also might start up if it's used in a background sync or a push
+// notification context.
 openDatabaseAndReplayRequests();
 
 self.addEventListener('activate', function(event) {
@@ -184,7 +202,12 @@ self.addEventListener('fetch', function(event) {
 });
 
 function checkForAnalyticsRequest(requestUrl) {
-  if (requestUrl.match(/\/\/www.google-analytics.com\/collect/i)) {
+  // Construct a URL object (https://developer.mozilla.org/en-US/docs/Web/API/URL.URL)
+  // to make it easier to check the various components without dealing with string parsing.
+  var url = new URL(requestUrl);
+
+  if ((url.hostname == 'www.google-analytics.com' || url.hostname == 'ssl.google-analytics.com') &&
+       url.pathname == '/collect') {
     console.log('  Storing Google Analytics request in IndexedDB to be replayed later.');
     saveAnalyticsRequest(requestUrl);
   }
