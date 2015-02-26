5cf768790c919dced7fe09b7dbb200731970245c
Matt Gaunt <matt@gauntface.co.uk>
2015-02-26 13:47:20 +0100

    Fixing up the pushsubscriptionchange callback

--- push-messaging-and-notifications/service-worker.js
+++ push-messaging-and-notifications/service-worker.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function sendToServer() {
+  // TODO: Implement ;)
+}
+
 self.addEventListener('push', function(event) {
   console.log('Received a push message', event);
 
@@ -17,8 +21,22 @@ self.addEventListener('push', function(event) {
   );
 });
 
-self.addEventListener('pushsubscriptionlost', function(event) {
-  console.log('Push subscription lost' + event);
+self.addEventListener('pushsubscriptionchange', function(event) {
+  if(Notification.permission !== 'granted') {
+    // We need to ask the user to enable notifications
+
+    // You may want to remove any previous subscription details
+    // from your server since the user won't receive any of the
+    // push messages
+    return;
+  } 
+  event.waitUntil(self.registration.pushManager.subscribe().then(function(subscription) {
+      // TODO: Send subscriptionId and endpoint to your server
+      // so that you can send a push message at a later date
+      return sendToServer(subscription);
+    }).catch(function(error) {
+      console.warn('Unable to subscribe user to push notifications');
+    }));
 });
 
 self.addEventListener('notificationclick', function(event) {
