cccb30db48b21d867d1b439a7d7a1daaa16d1930
Addy Osmani <addyosmani@gmail.com>
2015-04-14 11:27:55 +0100

    Permissions: address feedback

--- permissions/sw-focus.js
+++
@@ -1,9 +0,0 @@
-self.onmessage = function(event) {
-  console.log('message received ' + event);
-  self.clients.getAll().then(function(c) {
-    console.log(c);
-    c[0].focus();
-  });
-};
-
-console.log('foobar');
