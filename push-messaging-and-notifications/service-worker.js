fbae1cbb54760000488a19b64f2508b417ae241f
Joe Medley <jmedley@google.com>
2015-06-25 10:41:37 +0100

    Update service-worker.js
    The test client.url == '/' doesn't work in every instance. I'll be happy to put something else if anyone has an idea.

--- push-messaging-and-notifications/service-worker.js
+++ push-messaging-and-notifications/service-worker.js
@@ -31,7 +31,7 @@ self.addEventListener('notificationclick', function(event) {
   }).then(function(clientList) {
     for (var i = 0; i < clientList.length; i++) {
       var client = clientList[i];
-      if (client.url == '/' && 'focus' in client)
+      if ('focus' in client)
         return client.focus();
     }
     if (clients.openWindow)
