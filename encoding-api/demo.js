9be4d14f75b5ca020e0342a18fb80ef94f725288
Jeff Posnick <jeffy@google.com>
2015-10-13 20:35:32 -0400

    Template migration

---
+++ encoding-api/demo.js
@@ -0,0 +1,36 @@
+if ('TextDecoder' in window) {
+  // The local files to be fetched, mapped to the encoding that they're using.
+  var filesToEncoding = {
+    'utf8.bin': 'utf-8',
+    'utf16le.bin': 'utf-16le',
+    'macintosh.bin': 'macintosh'
+  };
+
+  Object.keys(filesToEncoding).forEach(function(file) {
+    fetchAndDecode(file, filesToEncoding[file]);
+  });
+} else {
+  ChromeSamples.setStatus('Your browser does not support the Encoding API.');
+}
+
+// Use XHR to fetch `file` and interpret its contents as being encoded with `encoding`.
+function fetchAndDecode(file, encoding) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', file);
+  // Using 'arraybuffer' as the responseType ensures that the raw data is returned,
+  // rather than letting XMLHttpRequest decode the data first.
+  xhr.responseType = 'arraybuffer';
+  xhr.onload = function() {
+    if (this.status == 200) {
+      // The decode() method takes a DataView as a parameter, which is a wrapper on top of the ArrayBuffer.
+      var dataView = new DataView(this.response);
+      // The TextDecoder interface is documented at http://encoding.spec.whatwg.org/#interface-textdecoder
+      var decoder = new TextDecoder(encoding);
+      var decodedString = decoder.decode(dataView);
+      ChromeSamples.log(decodedString);
+    } else {
+      ChromeSample.setStatus('Error while requesting', file, this.status);
+    }
+  };
+  xhr.send();
+}
