360f5a991736426e8c2b18fbab772729a2137558
Francois Beaufort <beaufort.francois@gmail.com>
2015-11-04 14:08:47 +0100

    Added use strict to bluetooth notifications sample

--- web-bluetooth/notifications.js
+++ web-bluetooth/notifications.js
@@ -1,12 +1,13 @@
-var myCharacteristic;
+'use strict';
+let myCharacteristic;
 
 function onStartButtonClick() {
-  var serviceUuid = document.getElementById('service').value;
+  let serviceUuid = document.getElementById('service').value;
   if (serviceUuid.startsWith('0x')) {
     serviceUuid = parseInt(serviceUuid, 16);
   }
 
-  var characteristicUuid = document.getElementById('characteristic').value;
+  let characteristicUuid = document.getElementById('characteristic').value;
   if (characteristicUuid.startsWith('0x')) {
     characteristicUuid = parseInt(characteristicUuid, 16);
   }
@@ -38,8 +39,8 @@ function onStopButtonClick() {
 }
 
 function handleNotifications(event) {
-  var buffer = event.target.value;
-  var data = new DataView(buffer);
+  let buffer = event.target.value;
+  let data = new DataView(buffer);
   // Convert raw data bytes to hex values just for the sake of showing something.
   // In the "real" world, you'd use data.getUint8, data.getUint16 or even
   // TextDecoder to process raw data bytes.
