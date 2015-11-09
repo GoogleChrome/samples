51a5c8ba4e5b999281cd082744ba36b73f944672
Francois Beaufort <beaufort.francois@gmail.com>
2015-11-09 10:33:48 +0100

    Added new filters to web-bluetooth device-info

--- web-bluetooth/device-info.js
+++ web-bluetooth/device-info.js
@@ -1,13 +1,28 @@
 function onFormSubmit() {
   'use strict';
 
+  let options = { filters : []};
+
   let filterService = document.getElementById('service').value;
   if (filterService.startsWith('0x')) {
     filterService = parseInt(filterService, 16);
   }
+  if (filterService) {
+    options.filters.push({services: [ filterService ]});
+  }
+
+  let filterName = document.getElementById('name').value;
+  if (filterName) {
+    options.filters.push({name: filterName});
+  }
+
+  let filterNamePrefix = document.getElementById('namePrefix').value;
+  if (filterNamePrefix) {
+    options.filters.push({namePrefix: filterNamePrefix});
+  }
 
   log('Requesting Bluetooth Device...');
-  navigator.bluetooth.requestDevice({filters:[{services:[ filterService ]}]})
+  navigator.bluetooth.requestDevice(options)
   .then(device => {
     log('> Device Name:   ' + device.name);
     log('> Device Id:     ' + device.id);
