30e66100c99351cc79a5c7c3744e290083d591c7
Jeff Posnick <jeffy@google.com>
2015-11-24 17:08:30 -0500

    IndexedDB getAll() sample

---
+++ idb-getall/README.md
@@ -0,0 +1,5 @@
+IndexedDB getAll() Methods Sample
+===
+See https://googlechrome.github.io/samples/idb-getall/index.html for a live demo.
+
+Learn more at https://www.chromestatus.com/feature/6537756637396992
---
+++ idb-getall/demo.js
@@ -0,0 +1,46 @@
+var DB_NAME = 'timestamps';
+var DB_VERSION = 1;
+var STORE_NAME = 'store';
+var dbInstance;
+
+// Simple Promise-based wrapper around getting an IndexedDB instance.
+function getDB() {
+  if (dbInstance) {
+    return Promise.resolve(dbInstance);
+  }
+
+  return new Promise(function(resolve) {
+    var request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onupgradeneeded = function() {
+      request.result.createObjectStore(STORE_NAME, {autoIncrement: true});
+    };
+
+    request.onsuccess = function() {
+      dbInstance = request.result;
+      resolve(dbInstance);
+    };
+  });
+}
+
+
+document.querySelector('#add').addEventListener('click', function(event) {
+  getDB().then(function(db) {
+    var transaction = db.transaction(STORE_NAME, 'readwrite');
+    var objectStore = transaction.objectStore(STORE_NAME);
+    objectStore.put(Date.now());
+  });
+});
+
+document.querySelector('#display').addEventListener('click', function(event) {
+  getDB().then(function(db) {
+    var transaction = db.transaction(STORE_NAME, 'readonly');
+    var objectStore = transaction.objectStore(STORE_NAME);
+    // IDBObjectStore.getAll() will return the full set of items in our store.
+    objectStore.getAll().onsuccess = function(event) {
+      var timestamps = event.target.result;
+      ChromeSamples.setStatus('There are ' + timestamps.length + ' timestamps saved in IndexedDB: '
+        + timestamps.join(', '));
+    };
+  });
+});
