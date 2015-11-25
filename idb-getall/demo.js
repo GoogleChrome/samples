733190d8e6a09c7f538043b0a5e23951e03da38a
Jeff Posnick <jeffy@google.com>
2015-11-25 09:38:45 -0500

    Refactoring to remove the Promise wrapper.

--- idb-getall/demo.js
+++ idb-getall/demo.js
@@ -1,50 +1,41 @@
+var addButton = document.querySelector('#add');
+var displayButton = document.querySelector('#display');
+
 var DB_NAME = 'timestamps';
 var DB_VERSION = 1;
 var STORE_NAME = 'store';
-var dbInstance;
+var db;
 
-// Simple Promise-based wrapper around getting an IndexedDB instance.
-function getDB() {
-  if (dbInstance) {
-    return Promise.resolve(dbInstance);
-  }
-
-  return new Promise(function(resolve) {
-    var request = indexedDB.open(DB_NAME, DB_VERSION);
-
-    request.onupgradeneeded = function() {
-      request.result.createObjectStore(STORE_NAME, {autoIncrement: true});
-    };
-
-    request.onsuccess = function() {
-      dbInstance = request.result;
-      resolve(dbInstance);
-    };
+var request = indexedDB.open(DB_NAME, DB_VERSION);
+request.onupgradeneeded = function() {
+  // Create a new object store if this is the first time we're using
+  // this DB_NAME/DB_VERSION combo.
+  request.result.createObjectStore(STORE_NAME, {autoIncrement: true});
+};
+request.onsuccess = function() {
+  db = request.result;
+  // Enable our buttons once the IndexedDB instance is available.
+  [addButton, displayButton].forEach(function(button) {
+    button.disabled = false;
   });
-}
+};
 
-
-document.querySelector('#add').addEventListener('click', function(event) {
-  getDB().then(function(db) {
-    var transaction = db.transaction(STORE_NAME, 'readwrite');
-    var objectStore = transaction.objectStore(STORE_NAME);
-    objectStore.put(Date.now());
-  }).catch(function(error) {
-    ChromeSamples.setStatus(error);
-  });
+addButton.addEventListener('click', function() {
+  var transaction = db.transaction(STORE_NAME, 'readwrite');
+  var objectStore = transaction.objectStore(STORE_NAME);
+  // Add the current timestamp to IndexedDB.
+  objectStore.put(Date.now()).onsuccess = function() {
+    ChromeSamples.setStatus('Added timestamp to IndexedDB.');
+  };
 });
 
-document.querySelector('#display').addEventListener('click', function(event) {
-  getDB().then(function(db) {
-    var transaction = db.transaction(STORE_NAME, 'readonly');
-    var objectStore = transaction.objectStore(STORE_NAME);
-    // IDBObjectStore.getAll() will return the full set of items in our store.
-    objectStore.getAll().onsuccess = function(event) {
-      var timestamps = event.target.result;
-      ChromeSamples.setStatus('There are ' + timestamps.length + ' timestamps saved in IndexedDB: '
-        + timestamps.join(', '));
-    };
-  }).catch(function(error) {
-    ChromeSamples.setStatus(error);
-  });
+displayButton.addEventListener('click', function() {
+  var transaction = db.transaction(STORE_NAME, 'readonly');
+  var objectStore = transaction.objectStore(STORE_NAME);
+  // IDBObjectStore.getAll() will return the full set of items in our store.
+  objectStore.getAll().onsuccess = function(event) {
+    var timestamps = event.target.result;
+    ChromeSamples.setStatus('There are ' + timestamps.length +
+      ' timestamps saved in IndexedDB: ' + timestamps.join(', '));
+  };
 });
