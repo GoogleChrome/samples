e334935e10289be929573f3b6163244b82a96b5b
Jeff Posnick <jeffy@google.com>
2015-11-30 13:45:45 -0500

    Feedback.

--- font-face-set/demo.js
+++ font-face-set/demo.js
@@ -1,18 +1,18 @@
-// Load Bitter via JavaScript, as an alternative to CSS's @font-face rule.
+function logLoaded(fontFace) {
+  ChromeSamples.log(fontFace.family, 'loaded successfully.');
+}
+
+// Loading FontFaces via JavaScript is alternative to using CSS's @font-face rule.
 var bitterFontFace = new FontFace('Bitter', 'url(https://fonts.gstatic.com/s/bitter/v7/HEpP8tJXlWaYHimsnXgfCOvvDin1pK8aKteLpeZ5c0A.woff2)');
 document.fonts.add(bitterFontFace);
+bitterFontFace.loaded.then(logLoaded);
+var oxygenFontFace = new FontFace('Oxygen', 'url(https://fonts.gstatic.com/s/oxygen/v5/qBSyz106i5ud7wkBU-FrPevvDin1pK8aKteLpeZ5c0A.woff2)');
+document.fonts.add(oxygenFontFace);
+oxygenFontFace.loaded.then(logLoaded);
 
 // The .ready promise resolves when all fonts that have been previously requested
 // are loaded and layout operations are complete.
 document.fonts.ready.then(function() {
-  // Our CSS sets visibility: hidden on the oxygen class initially,
-  // so let's make it visible now.
-  // This technique avoids displaying text until the font it requires is loaded.
-  // An alternative approach is used with the text styled with the bitter class,
-  // which is visible from the start using the default font,
-  // and then switches to the custom Bitter font once that's loaded.
-  document.querySelector('.oxygen').style.visibility = 'visible';
-
   ChromeSamples.log('There are', document.fonts.size, 'FontFaces loaded.\n');
 
   // document.fonts has a Set-like interface. Here, we're iterating over its values.
