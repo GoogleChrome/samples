3c30fb50f723ca6721af8a48ce455ea83917ed96
Jeffrey Posnick <jeffy@google.com>
2016-04-14 14:57:09 -0400

    Check to see if KeyboardEvent.code is supported

--- keyboardevent-code-attribute/demo.js
+++ keyboardevent-code-attribute/demo.js
@@ -1,3 +1,7 @@
 window.addEventListener('keydown', function(event) {
-  ChromeSamples.log('KeyboardEvent.code:', event.code);
+  if ('code' in event) {
+    ChromeSamples.log('KeyboardEvent.code:', event.code);
+  } else {
+    ChromeSamples.setStatus('KeyboardEvent.code is not supported.');
+  }
 });
