009afd56d879a09c448e0a95ad1cd12e296e5394
Sam Dutton <samdutton@gmail.com>
2016-05-25 19:08:23 +0100

    Added check for range requests

--- service-worker/prefetch-video/service-worker.js
+++ service-worker/prefetch-video/service-worker.js
@@ -32,7 +32,9 @@ var CURRENT_CACHES = {
 self.addEventListener('install', function(event) {
   var now = Date.now();
 
-  var urlsToPrefetch = ['static/video.webm', 'static/poster.jpg'];
+  var urlsToPrefetch = [
+  'static/video.webm', 'static/poster.jpg'
+  ];
 
   // All of these logging statements should be visible via the "Inspect" interface
   // for the relevant SW accessed via chrome://serviceworker-internals
@@ -110,25 +112,55 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   console.log('Handling fetch event for', event.request.url);
-  var pos =
+
+  if (event.request.headers.get('range')) {
+    var pos =
     Number(/^bytes\=(\d+)\-$/g.exec(event.request.headers.get('range'))[1]);
-  console.log('event.request.headers', event.request.headers);
-  event.respondWith(
-    caches.open(CURRENT_CACHES.prefetch)
-    .then(function(cache) {
-      return cache.match(event.request.url);
-    }).then(function(res) {
-      return res.arrayBuffer();
-    }).then(function(ab) {
-      return new Response(
-        ab.slice(pos),
-        {
-          status: 206,
-          statusText: 'Partial Content',
-          headers: [
-          ['Content-Type', 'video/mp4'],
-          ['Content-Range', 'bytes ' + pos + '-' +
-            (ab.byteLength - 1) + '/' + ab.byteLength]]
-        });
-    }));
+    console.log('Range request, starting position:', pos);
+    event.respondWith(
+      caches.open(CURRENT_CACHES.prefetch)
+      .then(function(cache) {
+        return cache.match(event.request.url);
+      }).then(function(res) {
+        return res.arrayBuffer();
+      }).then(function(ab) {
+        return new Response(
+          ab.slice(pos),
+          {
+            status: 206,
+            statusText: 'Partial Content',
+            headers: [
+//            ['Content-Type', 'video/webm'],
+              ['Content-Range', 'bytes ' + pos + '-' +
+              (ab.byteLength - 1) + '/' + ab.byteLength]]
+          });
+      }));
+  } else {
+    event.respondWith(
+    // caches.match() will look for a cache entry in all of the caches available to the service worker.
+    // It's an alternative to first opening a specific named cache and then matching on that.
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found response in cache:', response);
+        return response;
+      }
+      console.log('No response found in cache. About to fetch from network...');
+      // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+      // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+      return fetch(event.request).then(function(response) {
+        console.log('Response from network is:', response);
+
+        return response;
+      }).catch(function(error) {
+        // This catch() will handle exceptions thrown from the fetch() operation.
+        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+        // It will return a normal response object that has the appropriate error code set.
+        console.error('Fetching failed:', error);
+
+        throw error;
+      });
+    })
+    );
+  }
 });
+
