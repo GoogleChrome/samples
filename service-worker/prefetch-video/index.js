ca3008e4147edc20b4ab9b1ffbe638d871a9260d
Sam Dutton <samdutton@gmail.com>
2016-06-06 15:05:01 +0100

    Fixed error message glitch

--- service-worker/prefetch-video/index.js
+++ service-worker/prefetch-video/index.js
@@ -1,11 +1,11 @@
-// Helper function which returns a promise which resolves once the service worker registration
-// is past the "installing" state.
+// Helper function which returns a promise which resolves once the
+// service worker registration is past the "installing" state.
 function waitUntilInstalled(registration) {
   return new Promise(function(resolve, reject) {
     if (registration.installing) {
-      // If the current registration represents the "installing" service worker, then wait
-      // until the installation step (during which the resources are pre-fetched) completes
-      // to display the file list.
+      // If the current registration represents the "installing" service worker,
+      // then wait until the installation step (during which the resources are
+      // pre-fetched) completes to display the file list.
       registration.installing.addEventListener('statechange', function(e) {
         if (e.target.state === 'installed') {
           resolve();
@@ -14,9 +14,9 @@ function waitUntilInstalled(registration) {
         }
       });
     } else {
-      // Otherwise, if this isn't the "installing" service worker, then installation must have been
-      // completed during a previous visit to this page, and the resources are already pre-fetched.
-      // So we can show the list of files right away.
+      // Otherwise, if this isn't the "installing" service worker, then
+      // installation must have been completed during a previous visit to this
+      // page, and the resources are already pre-fetched.
       resolve();
     }
   });
@@ -27,20 +27,13 @@ if ('serviceWorker' in navigator) {
     scope: './'
   })
     .then(waitUntilInstalled)
-    // .then(showFilesList)
     .catch(function(error) {
       // Something went wrong during registration. The service-worker.js file
       // might be unavailable or contain a syntax error.
-      document.querySelector('#status').textContent = error;
+      console.log(error);
     });
 } else {
-  // The current browser doesn't support service workers.
-  var aElement = document.createElement('a');
-  aElement.href =
-    'http://www.chromium.org/blink/serviceworker/service-worker-faq';
-  aElement.textContent =
-    'Service workers are not supported in the current browser.';
-  document.querySelector('#status').appendChild(aElement);
+  console.log('This browser does not support Service Worker');
 }
 
 document.querySelector('video').onloadedmetadata = function() {
