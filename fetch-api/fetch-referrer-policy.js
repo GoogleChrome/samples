3e4f9fdcf1deec548b0768afba47f1d6e8e89ad3
Jeff Posnick <jeffy@google.com>
2016-06-09 14:00:31 -0400

    Review feedback.

--- fetch-api/fetch-referrer-policy.js
+++ fetch-api/fetch-referrer-policy.js
@@ -1,9 +1,22 @@
-var baseUrl = 'users.json';
-var policies = ['no-referrer', 'no-referrer-when-downgrade', 'origin',
-  'origin-when-cross-origin', 'unsafe-url'];
+var policies = [
+  // Referer will never be set.
+  'no-referrer',
+
+  // Referer will not be set when navigating from HTTPS to HTTP.
+  'no-referrer-when-downgrade',
+
+  // Referer will be set to just the origin, omitting the URL's path and search.
+  'origin',
+
+  // Full Referer for same-origin requests, and bare origin for cross-origin requests.
+  'origin-when-cross-origin',
+
+  // Full Referer for all requests, whether same- or cross-origin.
+  'unsafe-url'
+];
+var url = 'users.json';
 
 policies.forEach(policy => {
-  var url = `${baseUrl}?policy=${policy}`;
   ChromeSamples.log(`Fetching ${url}...`);
   fetch(url, {referrerPolicy: policy});
 });
