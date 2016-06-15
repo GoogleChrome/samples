a3912f0cd274b64dddf38fda40e218eba098c7e6
François Beaufort <beaufortfrancois@users.noreply.github.com>
2016-06-15 10:14:07 +0200

    bluetooth: Add get-primary-services sample (#364)
    * bluetooth: Add get-primary-services sample
    
    * s/19/12/
    
    * FREEZE.unindexed
    
    * Address Gio's feedback
    
    * Address Gio's feedback
    
    * Added getSupportedProperties util function

---
+++ web-bluetooth/discover-services-and-characteristics.js
@@ -0,0 +1,60 @@
+function onButtonClick() {
+  // Validate services UUID entered by user first.
+  let services = document.querySelector('#optionalServices').value.split(/, ?/)
+    .map(s => s.startsWith('0x') ? parseInt(s) : s)
+    .filter(s => s && BluetoothUUID.getService);
+
+  log('Requesting any Bluetooth Device...');
+  navigator.bluetooth.requestDevice({
+    filters: anyDevice(),
+    optionalServices: services
+  })
+  .then(device => {
+    log('Connecting to GATT Server...');
+    return device.gatt.connect();
+  })
+  .then(server => {
+    if (!services.length) {
+      return Promise.reject('Please enter some services first.');
+    }
+    // Note that we could also get all services that match a specific UUID by
+    // passing it to getPrimaryServices().
+    log('Getting Services...');
+    return server.getPrimaryServices();
+  })
+  .then(services => {
+    log('Getting Characteristics...');
+    services.forEach(service => {
+      service.getCharacteristics().then(characteristics => {
+        log('> Service: ' + service.uuid);
+        characteristics.forEach(characteristic => {
+          log('>> Characteristic: ' + characteristic.uuid + ' ' +
+              getSupportedProperties(characteristic));
+        });
+      });
+    });
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+/* Utils */
+
+function getSupportedProperties(characteristic) {
+  let supportedProperties = [];
+  for (const p in characteristic.properties) {
+    if (characteristic.properties[p] === true) {
+      supportedProperties.push(p.toUpperCase());
+    }
+  }
+  return '[' + supportedProperties.join(', ') + ']';
+}
+
+function anyDevice() {
+  // This is the closest we can get for now to get all devices.
+  // https://github.com/WebBluetoothCG/web-bluetooth/issues/234
+  return Array.from('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+      .map(c => ({namePrefix: c}))
+      .concat({name: ''});
+}
