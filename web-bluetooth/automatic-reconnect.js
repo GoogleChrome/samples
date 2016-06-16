8ab0e1ef5d44dc135d1d0da891b060e97b324819
Francois Beaufort <beaufort.francois@gmail.com>
2016-06-16 08:25:57 +0200

    bluetooth: Add automatic reconnect sample

---
+++ web-bluetooth/automatic-reconnect.js
@@ -0,0 +1,64 @@
+var bluetoothDevice;
+
+function onButtonClick() {
+  bluetoothDevice = null;
+  log('Requesting any Bluetooth Device...');
+  navigator.bluetooth.requestDevice({filters: anyDevice()})
+  .then(device => {
+    bluetoothDevice = device;
+    bluetoothDevice.addEventListener('gattserverdisconnected', onDisconnected);
+    connect();
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+function connect() {
+  exponentialBackoff(3 /* max retries */, 2 /* seconds delay */,
+    function toTry() {
+      time('Connecting to Bluetooth Device... ');
+      return bluetoothDevice.gatt.connect();
+    },
+    function success() {
+      log('> Bluetooth Device connected. Try disconnect it now.');
+    },
+    function fail() {
+      time('Failed to reconnect.');
+    });
+}
+
+function onDisconnected() {
+  log('> Bluetooth Device disconnected');
+  connect();
+}
+
+/* Utils */
+
+// This function keeps calling "toTry" until promise resolves or has
+// retried "max" number of times. First retry has a delay of "delay" seconds.
+// "success" is called upon success.
+function exponentialBackoff(max, delay, toTry, success, fail) {
+  toTry().then(result => success(result))
+  .catch(_ => {
+    if (max === 0) {
+      return fail();
+    }
+    time('Retrying in ' + delay + 's... (' + max + ' tries left)');
+    setTimeout(function() {
+      exponentialBackoff(--max, delay * 2, toTry, success, fail);
+    }, delay * 1000);
+  });
+}
+
+function anyDevice() {
+  // This is the closest we can get for now to get all devices.
+  // https://github.com/WebBluetoothCG/web-bluetooth/issues/234
+  return Array.from('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+      .map(c => ({namePrefix: c}))
+      .concat({name: ''});
+}
+
+function time(text) {
+  log('[' + new Date().toJSON().substr(11, 8) + '] ' + text);
+}
