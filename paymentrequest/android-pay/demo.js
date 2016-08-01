1e273428eccc99b93b313539bea7e87de35cb29a
Rouslan Solomakhin <rouslan.solomakhin@gmail.com>
2016-08-01 07:37:34 -0700

    Clarifying the code samples in responds to feedback from developers. (#396)

--- paymentrequest/android-pay/demo.js
+++ paymentrequest/android-pay/demo.js
@@ -1,22 +1,23 @@
+/**
+ * Invokes PaymentRequest for Android Pay.
+ */
 function onBuyClicked() {
-  var supportedInstruments = [
-    {
-      supportedMethods: ['https://android.com/pay'],
-      data: {
-        merchantId: '123456',
-        allowedCardNetworks: ['AMEX', 'MASTERCARD', 'VISA'],
-        paymentMethodTokenizationParameters: {
-          tokenizationType: 'GATEWAY_TOKEN',
-          parameters: {
-            'gateway': 'stripe',
-            // Place your own Stripe publishable key here.
-            'stripe:publishableKey': 'pk_test_VKUbaXb3LHE7GdxyOBMNwXqa',
-            'stripe:version': '2016-03-07'
-          }
+  var supportedInstruments = [{
+    supportedMethods: ['https://android.com/pay'],
+    data: {
+      environment: 'TEST',
+      merchantId: '123456',
+      allowedCardNetworks: ['AMEX', 'DISCOVER', 'MASTERCARD', 'VISA'],
+      paymentMethodTokenizationParameters: {
+        tokenizationType: 'GATEWAY_TOKEN',
+        parameters: {
+          'gateway': 'stripe',
+          'stripe:publishableKey': '<PLACE_YOUR_STRIPE_PUBLISHABLE_KEY_HERE>',
+          'stripe:version': '2016-03-07'
         }
       }
     }
-  ];
+  }];
 
   var details = {
     total: {label: 'Donation', amount: {currency: 'USD', value: '55.00'}},
@@ -32,30 +33,52 @@ function onBuyClicked() {
     ]
   };
 
-  try {
-    new PaymentRequest(supportedInstruments, details) // eslint-disable-line no-undef
-        .show()
-        .then(function(instrumentResponse) {
-          // Simulate server-side processing with a 2 second delay.
-          window.setTimeout(function() {
-            instrumentResponse.complete('success')
-                .then(function() {
-                  document.getElementById('result').innerHTML =
-                      'methodName: ' + instrumentResponse.methodName +
-                      '<br>details: ' +
-                      JSON.stringify(instrumentResponse.details, undefined, 2);
-                })
-                .catch(function(err) {
-                  ChromeSamples.setStatus(err.message);
-                });
-          }, 2000);
+  new PaymentRequest(supportedInstruments, details) // eslint-disable-line no-undef
+      .show()
+      .then(function(instrumentResponse) {
+        sendPaymentToServer(instrumentResponse);
+      })
+      .catch(function(err) {
+        ChromeSamples.setStatus(err);
+      });
+}
+
+/**
+ * Simulates processing the payment data on the server.
+ *
+ * @param {PaymentResponse} instrumentResponse The payment information to
+ * process.
+ */
+function sendPaymentToServer(instrumentResponse) {
+  // There's no server-side component of these samples. Not transactions are
+  // processed and no money exchanged hands. Instantaneous transactions are not
+  // realistic. Add a 2 second delay to make it seem more real.
+  window.setTimeout(function() {
+    instrumentResponse.complete('success')
+        .then(function() {
+          document.getElementById('result').innerHTML =
+              instrumentToJsonString(instrumentResponse);
         })
         .catch(function(err) {
-          ChromeSamples.setStatus(err.message);
+          ChromeSamples.setStatus(err);
         });
-  } catch (e) {
-    ChromeSamples.setStatus('Developer mistake: \'' + e.message + '\'');
-  }
+  }, 2000);
+}
+
+/**
+ * Converts the payment instrument into a JSON string.
+ *
+ * @private
+ * @param {PaymentResponse} instrument The instrument to convert.
+ * @return {string} The JSON string representation of the instrument.
+ */
+function instrumentToJsonString(instrument) {
+  // PaymentInsrument is an interface, but JSON.stringify works only on
+  // dictionaries.
+  return JSON.stringify({
+    methodName: instrument.methodName,
+    details: instrument.details
+  }, undefined, 2);
 }
 
 var buyButton = document.getElementById('buyButton');
--- paymentrequest/credit-cards/demo.js
+++ paymentrequest/credit-cards/demo.js
@@ -1,9 +1,10 @@
+/**
+ * Invokes PaymentRequest for credit cards.
+ */
 function onBuyClicked() {
   var supportedInstruments = [{
-    supportedMethods: [
-      'amex', 'diners', 'discover', 'jcb', 'maestro', 'mastercard', 'unionpay',
-      'visa'
-    ]
+    supportedMethods: ['amex', 'diners', 'discover', 'jcb', 'mastercard',
+        'unionpay', 'visa']
   }];
 
   var details = {
@@ -20,30 +21,56 @@ function onBuyClicked() {
     ]
   };
 
-  try {
-    new PaymentRequest(supportedInstruments, details) // eslint-disable-line no-undef
-        .show()
-        .then(function(instrumentResponse) {
-          // Simulate server-side processing with a 2 second delay.
-          window.setTimeout(function() {
-            instrumentResponse.complete('success')
-                .then(function() {
-                  document.getElementById('result').innerHTML =
-                      'methodName: ' + instrumentResponse.methodName +
-                      '<br>details: ' +
-                      JSON.stringify(instrumentResponse.details, undefined, 2);
-                })
-                .catch(function(err) {
-                  ChromeSamples.setStatus(err.message);
-                });
-          }, 2000);
+  new PaymentRequest(supportedInstruments, details) // eslint-disable-line no-undef
+      .show()
+      .then(function(instrumentResponse) {
+        sendPaymentToServer(instrumentResponse);
+      })
+      .catch(function(err) {
+        ChromeSamples.setStatus(err);
+      });
+}
+
+/**
+ * Simulates processing the payment data on the server.
+ *
+ * @param {PaymentResponse} instrumentResponse The payment information to
+ * process.
+ */
+function sendPaymentToServer(instrumentResponse) {
+  // There's no server-side component of these samples. Not transactions are
+  // processed and no money exchanged hands. Instantaneous transactions are not
+  // realistic. Add a 2 second delay to make it seem more real.
+  window.setTimeout(function() {
+    instrumentResponse.complete('success')
+        .then(function() {
+          document.getElementById('result').innerHTML =
+              instrumentToJsonString(instrumentResponse);
         })
         .catch(function(err) {
-          ChromeSamples.setStatus(err.message);
+          ChromeSamples.setStatus(err);
         });
-  } catch (e) {
-    ChromeSamples.setStatus('Developer mistake: \'' + e.message + '\'');
-  }
+  }, 2000);
+}
+
+/**
+ * Converts the payment instrument into a JSON string.
+ *
+ * @private
+ * @param {PaymentResponse} instrument The instrument to convert.
+ * @return {string} The JSON string representation of the instrument.
+ */
+function instrumentToJsonString(instrument) {
+  var details = instrument.details;
+  details.cardNumber = 'XXXX-XXXX-XXXX-' + details.cardNumber.substr(12);
+  details.cardSecurityCode = '***';
+
+  // PaymentInsrument is an interface, but JSON.stringify works only on
+  // dictionaries.
+  return JSON.stringify({
+    methodName: instrument.methodName,
+    details: details
+  }, undefined, 2);
 }
 
 var buyButton = document.getElementById('buyButton');
