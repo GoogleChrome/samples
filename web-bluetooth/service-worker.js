b146754a7ce297d1169891ad7ba26d898008a9f3
Francois Beaufort <beaufort.francois@gmail.com>
2016-08-23 09:19:54 +0200

    Address Jeffrey's feedback

--- web-bluetooth/service-worker.js
+++ web-bluetooth/service-worker.js
@@ -1,27 +1,24 @@
 function addToCache(request, networkResponse) {
   return caches.open('web-bluetooth')
-    .then((cache) => cache.put(request, networkResponse.clone()));
+    .then(cache => cache.put(request, networkResponse.clone()));
 }
 
 function getCacheResponse(request) {
-  return caches.open('web-bluetooth').then((cache) => {
+  return caches.open('web-bluetooth').then(cache => {
     return cache.match(request);
   });
 }
 
 function getNetworkOrCacheResponse(request) {
-  return new Promise((resolve) => {
-    fetch(request).then((networkResponse) => {
-      addToCache(request, networkResponse);
-      resolve(networkResponse);
-    }).catch(() => {
-      getCacheResponse(request).then((cacheResponse) => {
-        resolve(cacheResponse || Response.error());
-      });
-    });
+  return fetch(request).then(networkResponse => {
+    addToCache(request, networkResponse);
+    return networkResponse;
+  }).catch(_ => {
+    return getCacheResponse(request)
+      .then(cacheResponse => cacheResponse || Response.error());
   });
 }
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(getNetworkOrCacheResponse(event.request));
 });
