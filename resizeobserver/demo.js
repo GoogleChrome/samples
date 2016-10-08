d8f66856b1383d317a5ec2c958dafb2a8e4a5531
Surma <surma@surma.link>
2016-10-08 12:49:02 +0100

    Implement @atotic and @jeffposnick review

--- resizeobserver/demo.js
+++ resizeobserver/demo.js
@@ -2,12 +2,8 @@
 
 const ro = new ResizeObserver(entries => {
   for (let entry of entries) {
-    if (entry.contentRect.width < 250) {
-      entry.target.style.border = '3px solid red';
-    } else {
-      entry.target.style.border = 'none';
-    }
+    entry.target.classList.toggle('stripes', entry.contentRect.width < 250);
   }
 });
 // Only observe the green box (which is the third box).
-ro.observe(document.querySelector('.box:nth-child(3)'));
+ro.observe(document.querySelector('.box:nth-child(2)'));
