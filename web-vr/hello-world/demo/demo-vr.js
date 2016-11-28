10bc250c8e43cd4aae915042075482651220f1c6
Paul Lewis <github@aerotwist.com>
2016-11-28 16:21:55 +0000

    [WebVR] Fixes depth issue

--- web-vr/hello-world/demo/demo-vr.js
+++ web-vr/hello-world/demo/demo-vr.js
@@ -64,8 +64,8 @@ class DemoVR extends Demo {
       // Store the first display we find. A more production-ready version should
       // allow the user to choose from their available displays.
       this._vr.display = displays[0];
-      this._vr.depthNear = DemoVR.CAMERA_SETTINGS.near;
-      this._vr.depthFar = DemoVR.CAMERA_SETTINGS.far;
+      this._vr.display.depthNear = DemoVR.CAMERA_SETTINGS.near;
+      this._vr.display.depthFar = DemoVR.CAMERA_SETTINGS.far;
 
       this._createPresentationButton();
     });
