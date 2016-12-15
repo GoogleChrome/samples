c677f4bbd3ef4a76b3ab28996ba7f011cc1d332b
Daniel Murphy <dmurph@chromium.org>
2016-12-15 15:17:52 -0500

    Added clear button and more names

--- indexed_db_observers/demo.js
+++ indexed_db_observers/demo.js
@@ -1,16 +1,27 @@
 var dataField = document.querySelector('#data');
 var nameField = document.querySelector('#name');
 var incrementButton = document.querySelector('#incrementButton');
+var resetButton = document.querySelector('#resetButton');
 
 var dataNumber = 0;
 var db = null;
 
+var setDataNumber = function(number) {
+  dataNumber = number;
+  dataField.innerHTML = dataNumber;
+}
+
 var observer = new IDBObserver(function(changes) {
   var originTab = changes.records.get('name')[0].key.lower;
+  var dataChange = changes.records.get('data')[0];
+  if (dataChange.type == 'clear') {
+    ChromeSamples.log('Tab "' + originTab + '" reset the data to 0.');
+    setDataNumber(0);
+    return;
+  }
   var newDataValue = changes.records.get('data')[0].key.lower;
-  ChromeSamples.log('Tab "' + originTab + '" set the data value to ' + newDataValue);
-  dataNumber = newDataValue;
-  dataField.innerHTML = dataNumber;
+  ChromeSamples.log('Tab "' + originTab + '" set the data value to ' + newDataValue + '.');
+  setDataNumber(newDataValue);
 });
 
 incrementButton.addEventListener('click', function(event) {
@@ -21,12 +32,31 @@ incrementButton.addEventListener('click', function(event) {
   var transaction = db.transaction(['data', 'name'], 'readwrite');
   var dataStore = transaction.objectStore('data');
   var nameStore = transaction.objectStore('name');
-  
-  dataNumber++;
-  dataStore.put(dataNumber, dataNumber);
+
+  // We wait for the first success to signify we've been scheduled.
+  var request = nameStore.put(nameField.innerHTML, nameField.innerHTML);
+  request.onsuccess = function() {
+    var newDataNumber = dataNumber + 1;
+    dataStore.put(newDataNumber, newDataNumber);
+    transaction.oncomplete = function() {
+      setDataNumber(newDataNumber);
+    }
+  }
+});
+
+resetButton.addEventListener('click', function(event) {
+  if (db == null) {
+    ChromeSamples.log('Database connection still openning.');
+    return;
+  }
+  var transaction = db.transaction(['data', 'name'], 'readwrite');
+  var dataStore = transaction.objectStore('data');
+  var nameStore = transaction.objectStore('name');
+
   nameStore.put(nameField.innerHTML, nameField.innerHTML);
+  dataStore.clear();
   transaction.oncomplete = function() {
-    dataField.innerHTML = dataNumber;
+    setDataNumber(0);
   }
 });
 
@@ -36,27 +66,28 @@ openRequest.onupgradeneeded = function() {
   openRequest.result.createObjectStore('data');
 }
 openRequest.onerror = function() {
-    ChromeSamples.log('Error openning database: ', openRequest.error);
+  ChromeSamples.log('Error opening database: ', openRequest.error);
 }
 openRequest.onsuccess = function(event) {
   db = event.target.result;
   var transaction = db.transaction(['data', 'name'], 'readonly');
   // Observe starting after this transaction!
-  observer.observe(db, transaction, {operationTypes: ['put']});
+  observer.observe(db, transaction, { operationTypes: ['put', 'clear'] });
+
   // This is ugly because we have to use the keys. When value or
   // transaction support is available we will use that.
   var dataStore = transaction.objectStore('data');
   dataStore.getAllKeys().onsuccess = function(event) {
     var maxNumber = 0;
     if (event.target.result.length == 0) {
-      dataNumber = maxNumber;
-      dataField.innerHTML = dataNumber;
+      setDataNumber(0);
+      ChromeSamples.log('Database is empty, starting at 0.');
       return;
     }
     event.target.result.forEach(function(number) {
       maxNumber = maxNumber > number ? maxNumber : number;
-    })
-    dataNumber = maxNumber;
-   dataField.innerHTML = dataNumber;
+    });
+    setDataNumber(maxNumber);
+    ChromeSamples.log('Database has starting value of ' + maxNumber + '.');
   }
 }
