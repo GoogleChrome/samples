3d13b0bbb8cf8f6f8e4e35660783fcdde3082e9d
Francois Beaufort <beaufort.francois@gmail.com>
2017-03-30 12:06:30 +0200

    Used getSettings() and applyConstraints()

--- image-capture/update-camera-zoom.js
+++ image-capture/update-camera-zoom.js
@@ -3,24 +3,24 @@ navigator.mediaDevices.getUserMedia({video: true})
   document.querySelector('video').srcObject = mediaStream;
 
   const track = mediaStream.getVideoTracks()[0];
-  const imageCapture = new ImageCapture(track);
+  const capabilities = track.getCapabilities();
+  const settings = track.getSettings();
 
-  return imageCapture.getPhotoCapabilities()
-  .then(photoCapabilities => {
-    // Check whether zoom is supported or not.
-    if (!photoCapabilities.zoom.min && !photoCapabilities.zoom.max) {
-      return Promise.reject('Zoom is not supported by ' + track.label);
-    }
+  const input = document.querySelector('input[type="range"]');
 
-    // Map zoom to a slider element.
-    const input = document.querySelector('input[type="range"]');
-    input.min = photoCapabilities.zoom.min;
-    input.max = photoCapabilities.zoom.max;
-    input.step = photoCapabilities.zoom.step;
-    input.value = photoCapabilities.zoom.current;
-    input.oninput = function(event) {
-      imageCapture.setOptions({zoom: event.target.value});
-    }
-  });
+  // Check whether zoom is supported or not.
+  if (!('zoom' in capabilities)) {
+    return Promise.reject('Zoom is not supported by ' + track.label);
+  }
+
+  // Map zoom to a slider element.
+  input.min = capabilities.zoom.min;
+  input.max = capabilities.zoom.max;
+  input.step = capabilities.zoom.step;
+  input.value = settings.zoom;
+  input.oninput = function(event) {
+    track.applyConstraints({advanced: [ {zoom: event.target.value} ]});
+  }
+  input.hidden = false;
 })
 .catch(error => ChromeSamples.log('Argh!', error.name || error));
