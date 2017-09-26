3f7d01317a13dab878cc8d437486b59f2fd83642
Francois Beaufort <beaufort.francois@gmail.com>
2017-09-26 11:19:35 +0200

    Update Network Information sample

--- network-information/demo.js
+++ network-information/demo.js
@@ -1,20 +1,21 @@
-function logConnectionInfo() {
-  var properties = [
-    'type',
-    'effectiveType',
-    'downlinkMax',
-    'downlink',
-    'rtt'
-  ];
+navigator.connection.addEventListener('change', logNetworkInfo);
 
-  ChromeSamples.log('Current Connection Status:');
-  properties.forEach(function(property) {
-    ChromeSamples.log('  ' + property + ':' +
-      (navigator.connection[property] || '[unknown]'));
-  });
+function logNetworkInfo() {
+  // Network type that browser uses
+  log('         type: ' + navigator.connection.type);
+
+  // Effective bandwidth estimate
+  log('     downlink: ' + navigator.connection.downlink + 'Mb/s');
+
+  // Effective round-trip time estimate
+  log('          rtt: ' + navigator.connection.rtt + 'ms');
+
+  // Upper bound on the downlink speed of the first network hop
+  log('  downlinkMax: ' + navigator.connection.downlinkMax + 'Mb/s');
+
+  // Effective connection type determined using a combination of recently
+  // observed rtt and downlink values: ' +
+  log('effectiveType: ' + navigator.connection.effectiveType);
 }
 
-// Log the connection info once at startup.
-logConnectionInfo();
-// Log the new connection info whenever it changes.
-navigator.connection.addEventListener('change', logConnectionInfo);
+logNetworkInfo();
