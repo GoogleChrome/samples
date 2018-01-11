dae3c34c80000409966562a125809f0656dc3f22
Dominic Farolino <domfarolino@gmail.com>
2018-01-11 10:51:43 -0500

    Add newer referrer policies (#565)

--- fetch-api/fetch-referrer-policy.js
+++ fetch-api/fetch-referrer-policy.js
@@ -5,11 +5,22 @@ var policies = [
   // Referer will not be set when navigating from HTTPS to HTTP.
   'no-referrer-when-downgrade',
 
+  // Full Referer for same-origin requests, and no Referer for cross-origin requests.
+  'same-origin',
+
   // Referer will be set to just the origin, omitting the URL's path and search.
   'origin',
 
+  // Referer will be set to just the origin except when navigating from HTTPS to HTTP,
+  // in which case no Referer is sent.
+  'strict-origin',
+
   // Full Referer for same-origin requests, and bare origin for cross-origin requests.
   'origin-when-cross-origin',
+
+  // Full Referer for same-origin requests, and bare origin for cross-origin requests
+  // except when navigating from HTTPS to HTTP, in which case no Referer is sent.
+  'strict-origin-when-cross-origin',
 
   // Full Referer for all requests, whether same- or cross-origin.
   'unsafe-url'
