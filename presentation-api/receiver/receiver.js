d4ba07280fe13671f414bbf193833122a6e4d4d5
François Beaufort <beaufort.francois@gmail.com>
2018-01-12 10:46:13 +0100

    Update Presentation API samples (#564)
    * Update Presentation API samples
    
    * Address @mfoltzgoogle feedback

--- presentation-api/receiver/receiver.js
+++ presentation-api/receiver/receiver.js
@@ -1,65 +1,66 @@
-(() => {
-  let connectionIdx = 0;
-  let messageIdx = 0;
-  const fruitEmoji = {
-    'grapes':      '\u{1F347}',
-    'watermelon':  '\u{1F349}',
-    'melon':       '\u{1F348}',
-    'tangerine':   '\u{1F34A}',
-    'lemon':       '\u{1F34B}',
-    'banana':      '\u{1F34C}',
-    'pineapple':   '\u{1F34D}',
-    'green apple': '\u{1F35F}',
-    'apple':       '\u{1F34E}',
-    'pear':        '\u{1F350}',
-    'peach':       '\u{1F351}',
-    'cherries':    '\u{1F352}',
-    'strawberry':  '\u{1F353}'
-  };
+let connectionIdx = 0;
+let messageIdx = 0;
 
-  function addConnection(connection) {
-    connection.connectionId = ++connectionIdx;
-
-    addMessage('New connection #' + connectionIdx);
-
-    connection.onmessage = (message) => {
-      messageIdx++;
-      const messageObj = JSON.parse(message.data);
-      const logString = 'Message ' + messageIdx + ' from connection #' +
-          connection.connectionId + ': ' + messageObj.message;
-      addMessage(logString, messageObj.lang);
-      maybeSetFruit(messageObj.message.toLowerCase());
-      connection.send('Received message ' + messageIdx);
-    };
-
-    connection.onclose = (closeEvent) => {
-      addMessage('Connection #' + connection.connectionId + ' closed, reason = ' +
-                 closeEvent.reason + ', message = ' + closeEvent.message);
-    };
-  };
-
-  function addMessage(content, language) {
-    const listElt = document.querySelector("#message-list");
-    const listItem = document.createElement("LI");
-    if (language) listItem.lang = language;
-    listItem.textContent = content;
-    listElt.appendChild(listItem);
-  };
-
-  function maybeSetFruit(message) {
-    if (message in fruitEmoji) {
-      document.querySelector('#main').textContent = fruitEmoji[message.toLowerCase()];
-    }
-  };
-
-  if (navigator.presentation.receiver) {
-    navigator.presentation.receiver.connectionList.then(list => {
-      list.connections.map(connection => {
-        addConnection(connection);
-      });
-      list.onconnectionavailable = (evt) => {
-        addConnection(evt.connection);
-      };
+if (navigator.presentation.receiver) {
+  navigator.presentation.receiver.connectionList
+  .then(list => {
+    list.connections.map(connection => addConnection(connection));
+    list.addEventListener('connectionavailable', function(event) {
+      addConnection(event.connection);
     });
+  });
+}
+
+function addConnection(connection) {
+  connection.connectionId = ++connectionIdx;
+  addMessage('New connection #' + connectionIdx);
+
+  connection.addEventListener('message', function(event) {
+    messageIdx++;
+    const data = JSON.parse(event.data);
+    const logString = 'Message ' + messageIdx + ' from connection #' +
+        connection.connectionId + ': ' + data.message;
+    addMessage(logString, data.lang);
+    maybeSetFruit(data.message);
+    connection.send('Received message ' + messageIdx);
+  });
+
+  connection.addEventListener('close', function(event) {
+    addMessage('Connection #' + connection.connectionId + ' closed, reason = ' +
+        event.reason + ', message = ' + event.message);
+  });
+};
+
+/* Utils */
+
+const fruitEmoji = {
+  'grapes':      '\u{1F347}',
+  'watermelon':  '\u{1F349}',
+  'melon':       '\u{1F348}',
+  'tangerine':   '\u{1F34A}',
+  'lemon':       '\u{1F34B}',
+  'banana':      '\u{1F34C}',
+  'pineapple':   '\u{1F34D}',
+  'green apple': '\u{1F35F}',
+  'apple':       '\u{1F34E}',
+  'pear':        '\u{1F350}',
+  'peach':       '\u{1F351}',
+  'cherries':    '\u{1F352}',
+  'strawberry':  '\u{1F353}'
+};
+
+function addMessage(content, language) {
+  const listItem = document.createElement("li");
+  if (language) {
+    listItem.lang = language;
   }
-})();
+  listItem.textContent = content;
+  document.querySelector("#message-list").appendChild(listItem);
+};
+
+function maybeSetFruit(message) {
+  const fruit = message.toLowerCase();
+  if (fruit in fruitEmoji) {
+    document.querySelector('#main').textContent = fruitEmoji[fruit];
+  }
+};
