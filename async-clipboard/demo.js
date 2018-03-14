7f909d62d2be49805c0c29bb20ed6602b1c9c5c5
Jason Miller <developit@google.com>
2018-03-14 12:15:36 -0400

    PR feedback updates!

--- async-clipboard/demo.js
+++ async-clipboard/demo.js
@@ -1,27 +1,23 @@
-const copy = document.querySelector('#copy');
-const paste = document.querySelector('#paste');
-const out = document.querySelector('#out');
-
 /** Write contents of the textarea to the clipboard when clicking "Copy" */
-copy.onclick = () => {
-  navigator.clipboard.writeText(out.value)
-    .then(result => {
-      ChromeSamples.log('Text copied.' + result);
+document.querySelector('#copy').onclick = () => {
+  navigator.clipboard.writeText(document.querySelector('#out').value)
+    .then(() => {
+      ChromeSamples.log('Text copied.');
     })
-    .catch(err => {
-      ChromeSamples.log('Failed to copy text: ' + err);
+    .catch(() => {
+      ChromeSamples.log('Failed to copy text.');
     });
 };
 
 /** Read from clipboard when clicking the Paste button */
-paste.onclick = () => {
+document.querySelector('#paste').onclick = () => {
   navigator.clipboard.readText()
     .then(text => {
-      out.value = text;
+      document.querySelector('#out').value = text;
       ChromeSamples.log('Text pasted.');
     })
-    .catch(err => {
-      ChromeSamples.log('Failed to read from clipboard: ' + err);
+    .catch(() => {
+      ChromeSamples.log('Failed to read from clipboard.');
     });
 };
 
@@ -34,7 +30,7 @@ document.addEventListener('paste', e => {
 });
 
 /** Set up buttons to request permissions and display status: */
-document.querySelectorAll('data-permission').forEach(btn => {
+document.querySelectorAll('[data-permission]').forEach(btn => {
   const permission = btn.getAttribute('data-permission');
   navigator.permissions.query({name: permission})
     .then(status => {
@@ -42,9 +38,15 @@ document.querySelectorAll('data-permission').forEach(btn => {
         btn.setAttribute('data-state', status.state);
       };
       status.onchange();
+    })
+    .catch(() => {
+      btn.setAttribute('data-state', 'unavailable');
+      btn.title = 'Permissions API unavailable.';
     });
   btn.onclick = () => {
-    navigator.permissions.request({name: permission})
+    Promise.resolve().then(() => {
+      return navigator.permissions.request({name: permission});
+    })
       .then(status => {
         ChromeSamples.log('Permission: ' + status.state);
       })
