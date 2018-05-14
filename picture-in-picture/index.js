f17bd178d6460f860bb7fe8d8ffaf3b421cb893a
Francois Beaufort <beaufort.francois@gmail.com>
2018-05-14 16:14:31 +0200

    Fix nit

--- picture-in-picture/index.js
+++ picture-in-picture/index.js
@@ -1,35 +1,33 @@
-if (!document.pictureInPictureEnabled) {
-  log('Picture-in-Picture is disabled by system.');
-}
+togglePipButton.disabled = !document.pictureInPictureEnabled;
 
 togglePipButton.addEventListener('click', async function(event) {
   togglePipButton.disabled = true;
   try {
-    if (video === document.pictureInPictureElement) {
-      log('Exiting Picture-in-Picture...');
-      await document.exitPictureInPicture();
-    } else {
+    if (video !== document.pictureInPictureElement) {
       log('Requesting Picture-in-Picture...');
       const pipWindow = await video.requestPictureInPicture();
-      log('Picture-in-Picture window is displayed.');
+      log(`> Window size is ${pipWindow.width}x${pipWindow.height}`);
       pipWindow.addEventListener('resize', onPipWindowResize);
+    } else {
+      log('Exiting Picture-in-Picture...');
+      await document.exitPictureInPicture();
     }
   } catch(error) {
-    log(error.messsage);
+    log(`> Argh! ${error}`);
   } finally {
     togglePipButton.disabled = false;
   }
 });
 
-function onPipWindowResize(event) {
-  const pipWindow = event.target;
-  log(`Picture-in-Picture window size is ${pipWindow.width}x${pipWindow.height}`);
-}
-
 video.addEventListener('enterpictureinpicture', function(event) {
-  log('Video entered Picture-in-Picture.');
+  log('> Video entered Picture-in-Picture.');
 });
 
 video.addEventListener('leavepictureinpicture', function(event) {
-  log('Video left Picture-in-Picture.');
+  log('> Video left Picture-in-Picture.');
 });
+
+function onPipWindowResize(event) {
+  const pipWindow = event.target;
+  log(`> Window size is ${pipWindow.width}x${pipWindow.height}`);
+}
