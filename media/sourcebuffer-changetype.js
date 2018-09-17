27898ebd95b5701cb29629cf45a38f299df488fc
Francois Beaufort <beaufort.francois@gmail.com>
2018-09-17 09:01:25 +0200

    Fix nits and host files

--- media/sourcebuffer-changetype.js
+++ media/sourcebuffer-changetype.js
@@ -2,29 +2,37 @@ const mediaSource = new MediaSource();
 const audio = document.querySelector('audio');
 audio.src = URL.createObjectURL(mediaSource);
 
-mediaSource.addEventListener('sourceopen', function() {
-  URL.revokeObjectURL(audio.src);
-  feedAudioElement(0 /* index */);
-});
-
 const resources = [
-  { url: 'bear-opus.webm',     type: 'audio/webm; codecs="opus"' },
-  { url: 'bear-flac_frag.mp4', type: 'audio/mp4; codecs="flac"' }
+  {
+    url: 'https://storage.googleapis.com/media-session/bear-opus.webm',
+    type: 'audio/webm; codecs="opus"'
+  }, {
+    url: 'https://storage.googleapis.com/media-session/bear-flac_frag.mp4',
+    type: 'audio/mp4; codecs="flac"'
+  }
 ];
 
-async function feedAudioElement(index) {
-  if (index == resources.length) {
+mediaSource.addEventListener('sourceopen', function() {
+  URL.revokeObjectURL(audio.src);
+  feedAudioElement(0 /* resources index */);
+});
+
+async function feedAudioElement(resourcesIndex) {
+  if (resourcesIndex == resources.length) {
     mediaSource.endOfStream();
     return;
   }
 
   try {
     let sourceBuffer;
-    const resource = resources[index];
+    const resource = resources[resourcesIndex];
 
     if (mediaSource.sourceBuffers.length == 0) {
       log('Adding source buffer with type: ' + resource.type);
       sourceBuffer = mediaSource.addSourceBuffer(resource.type);
+      // The 'sequence' mode is used here purely for simplicity. A more
+      // sophisticated app would know which timestampOffset and appendWindow to
+      // use for each appendBuffer operation when splicing media.
       sourceBuffer.mode = 'sequence';
     } else {
       log('Changing source buffer type to: ' + resource.type);
@@ -40,11 +48,11 @@ async function feedAudioElement(index) {
     sourceBuffer.appendBuffer(data);
     sourceBuffer.addEventListener('updateend', function() {
       log('Source buffer updated');
-      feedAudioElement(++index);
+      feedAudioElement(++resourcesIndex);
     }, { once: true });
 
   } catch(error) {
     log('Argh! ' + error);
-    feedAudioElement(++index);
+    feedAudioElement(++resourcesIndex);
   }
 }
