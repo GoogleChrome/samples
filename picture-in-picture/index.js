6c5ddc7354d536d3aff3327118a3386356cd0ec8
Francois Beaufort <beaufort.francois@gmail.com>
2018-09-25 15:03:02 +0200

    Refactoring

--- picture-in-picture/index.js
+++ picture-in-picture/index.js
@@ -1,14 +1,15 @@
+let pipWindow;
+
 togglePipButton.addEventListener('click', async function(event) {
+  log('Toggling Picture-in-Picture...');
   togglePipButton.disabled = true;
   try {
-    if (video !== document.pictureInPictureElement) {
-      log('Requesting Picture-in-Picture...');
-      const pipWindow = await video.requestPictureInPicture();
-      pipWindow.addEventListener('resize', onPipWindowResize);
-    } else {
-      log('Exiting Picture-in-Picture...');
+
+    if (video !== document.pictureInPictureElement)
+      await video.requestPictureInPicture();
+    else
       await document.exitPictureInPicture();
-    }
+
   } catch(error) {
     log(`> Argh! ${error}`);
   } finally {
@@ -16,19 +17,25 @@ togglePipButton.addEventListener('click', async function(event) {
   }
 });
 
+// Note that this can happen if user clicked the "Toggle Picture-in-Picture"
+// button but also if user clicked some browser context menu or if
+// Picture-in-Picture was triggered automatically for instance.
 video.addEventListener('enterpictureinpicture', function(event) {
   log('> Video entered Picture-in-Picture');
 
-  const pipWindow = event.pictureInPictureWindow;
+  pipWindow = event.pictureInPictureWindow;
   log(`> Window size is ${pipWindow.width}x${pipWindow.height}`);
+
+  pipWindow.addEventListener('resize', onPipWindowResize);
 });
 
 video.addEventListener('leavepictureinpicture', function(event) {
   log('> Video left Picture-in-Picture');
+
+  pipWindow.removeEventListener('resize', onPipWindowResize);
 });
 
 function onPipWindowResize(event) {
-  const pipWindow = event.target;
   log(`> Window size changed to ${pipWindow.width}x${pipWindow.height}`);
 }
 
