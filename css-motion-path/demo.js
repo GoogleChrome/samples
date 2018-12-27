6292aa20302c005742f35cd8be7742f41c2d3c4a
Matt Rakow <marakow@microsoft.com>
2018-12-27 11:26:26 -0800

    Update CSS motion path demo to use current property names

--- css-motion-path/demo.js
+++ css-motion-path/demo.js
@@ -1,20 +1,20 @@
 var firstScissorHalf = document.querySelector('#firstScissorHalf');
 var secondScissorHalf = document.querySelector('#secondScissorHalf');
 
-var positionKeyframes = [{motionOffset: '0%'}, {motionOffset: '100%'}];
+var positionKeyframes = [{offsetDistance: '0%'}, {offsetDistance: '100%'}];
 var positionTiming = {duration: 12000, iterations: Infinity};
 firstScissorHalf.animate(positionKeyframes, positionTiming);
 secondScissorHalf.animate(positionKeyframes, positionTiming);
 
 var firstRotationKeyframes = [
-  {motionRotation: 'auto 0deg'},
-  {motionRotation: 'auto -45deg'},
-  {motionRotation: 'auto 0deg'}
+  {offsetRotate: 'auto 0deg'},
+  {offsetRotate: 'auto -45deg'},
+  {offsetRotate: 'auto 0deg'}
 ];
 var secondRotationKeyframes = [
-  {motionRotation: 'auto 0deg'},
-  {motionRotation: 'auto 45deg'},
-  {motionRotation: 'auto 0deg'}
+  {offsetRotate: 'auto 0deg'},
+  {offsetRotate: 'auto 45deg'},
+  {offsetRotate: 'auto 0deg'}
 ];
 var rotationTiming = {duration: 1000, iterations: Infinity};
 firstScissorHalf.animate(firstRotationKeyframes, rotationTiming);
