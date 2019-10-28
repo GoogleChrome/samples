3a904b7068d1793023a5e8338c11f677d99bbebd
Jeffrey Posnick <jeffy@google.com>
2019-10-28 14:59:23 -0400

    Update service-worker/custom-offline-page/service-worker.js
    Co-Authored-By: Thomas Steiner <tomac@google.com>

--- service-worker/custom-offline-page/service-worker.js
+++ service-worker/custom-offline-page/service-worker.js
@@ -21,7 +21,7 @@ const OFFLINE_URL = 'offline.html';
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    // Setting cache: reload in the new request will ensure that the response
+    // Setting {cache: 'reload'} in the new request will ensure that the response
     // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
   })());
