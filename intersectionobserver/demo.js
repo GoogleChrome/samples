18331d5a4736f677154bfb2fa984d11b426f12b9
Stefan Zager <szager@chromium.org>
2020-03-13 09:50:41 -0700

    s/forEach/some/

--- intersectionobserver/demo.js
+++ intersectionobserver/demo.js
@@ -14,23 +14,17 @@ function loadItems(n) {
 
 var intersectionObserver = new IntersectionObserver(entries => {
   // If the browser is busy while scrolling happens, multiple entries can
-  // accumulate between invocations of this callback, so we loop over
+  // accumulate between invocations of this callback. As long as any one
+  // of the notifications reports the sentinel within the scrolling viewport,
+  // we add more content.
   // them.
-  entries.forEach(entry => {
-    // If intersectionRatio is 0, the sentinel is out of view
-    // and we do not need to do anything.
-    if (entry.intersectionRatio > 0) {
-      loadItems(10);
-      // appendChild will move the existing element, so there is no need to
-      // remove it first.
-      scroller.appendChild(sentinel);
-      loadItems(5);
-      ChromeSamples.setStatus('Loaded up to item ' + counter);
-
-      // We have already loaded more content, no need to look at any more
-      // entries.
-      return;
-    }
-  });
+  if (entries.some(entry => entry.intersectionRatio > 0)) {
+    loadItems(10);
+    // appendChild will move the existing element, so there is no need to
+    // remove it first.
+    scroller.appendChild(sentinel);
+    loadItems(5);
+    ChromeSamples.setStatus('Loaded up to item ' + counter);
+  }
 });
 intersectionObserver.observe(sentinel);
