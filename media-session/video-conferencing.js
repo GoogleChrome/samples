58fdfb2f6819d7454ea674cdd940d67f561dd288
François Beaufort <beaufort.francois@gmail.com>
2021-04-28 14:03:27 +0200

    Add video conferencing actions media session sample (#716)

---
+++ media-session/video-conferencing.js
@@ -0,0 +1,68 @@
+let video = document.querySelector('video');
+
+let isMicrophoneActive = false;
+let isCameraActive = false;
+
+openCameraButton.addEventListener("click", async () => {
+  try {
+    const constraints = { video: true, audio: true };
+    video.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
+    // Activate the microphone and the camera.
+    isMicrophoneActive = true;
+    isCameraActive = true;
+    navigator.mediaSession.setMicrophoneActive(isMicrophoneActive);
+    navigator.mediaSession.setCameraActive(isCameraActive);
+  } catch (error) {
+    log(`> Argh! ${error}`);
+  }
+});
+
+togglePipButton.addEventListener("click", async () => {
+  try {
+    if (video !== document.pictureInPictureElement)
+      await video.requestPictureInPicture();
+    else await document.exitPictureInPicture();
+  } catch (error) {
+    log(`> Argh! ${error}`);
+  }
+});
+
+try {
+  navigator.mediaSession.setActionHandler('togglemicrophone', () => {
+    log('> User clicked "Toggle Mic" icon.');
+    // TODO: Handle muting or unmuting the microphone.
+    // Eventually update the microphone state.
+    isMicrophoneActive = !isMicrophoneActive;
+    navigator.mediaSession.setMicrophoneActive(isMicrophoneActive);
+  });
+} catch(error) {
+  log('Warning! The "togglemicrophone" media session action is not supported.');
+}
+
+try {
+  navigator.mediaSession.setActionHandler('togglecamera', () => {
+    log('> User clicked "Toggle Camera" icon.');
+    // TODO: Handle turning on or off the camera.
+    // Eventually update the camera state.
+    isCameraActive = !isCameraActive;
+    navigator.mediaSession.setCameraActive(isCameraActive);
+  });
+} catch(error) {
+  log('Warning! The "togglecamera" media session action is not supported.');
+}
+
+try {
+  navigator.mediaSession.setActionHandler("hangup", () => {
+    log('> User clicked "Hang Up" icon.');
+    // Stop video stream.
+    const tracks = video.srcObject.getTracks();
+    tracks.forEach(track => {
+      track.stop();
+    });
+    video.srcObject = null;
+    // Exit Picture-in-Picture.
+    document.exitPictureInPicture();
+  });
+} catch (error) {
+  log('Warning! The "hangup" media session action is not supported.');
+}
