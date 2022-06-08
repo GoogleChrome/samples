0a1f2f9b44e1e1bf59e09b4bbd16bd77573bedc2
rajababubula <rajababu.bula@hotmail.com>
2022-06-08 15:42:35 +0530

    Update index.js

--- web-nfc/index.js
+++ web-nfc/index.js
@@ -1,19 +1,15 @@
   scanButton.addEventListener("click", async () => {
   log("User clicked scan button");
-
   try {
     const ndef = new NDEFReader();
     await ndef.scan();
     log("> Scan started");
-
     ndef.addEventListener("readingerror", () => {
       log("Argh! Cannot read data from the NFC tag. Try another one?");
     });
-
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
       log(`> Serial Number: ${serialNumber}`);
-      log(`> Records: (${message.records.length})`);
-      
+      log(`> Records: (${message.records.length})`);    
       for (const record of message.records) {
           log(`\nRecord id: ${record.id}`);
           log(`Record type: ${record.recordType}`);
