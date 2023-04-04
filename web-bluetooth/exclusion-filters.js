8039064cf484b4eac82855302448c8e0b01d0da3
François Beaufort <beaufort.francois@gmail.com>
2023-04-04 10:33:52 +0000

    Add exclusion filters web bluetooth sample

---
+++ web-bluetooth/exclusion-filters.js
@@ -0,0 +1,43 @@
+function onButtonClick() {
+  let filters = populateFiltersById('filters');
+  let exclusionFilters = populateFiltersById('exclusionFilters');
+  let options = { filters, exclusionFilters };
+
+  log('Requesting Bluetooth Device...');
+  log('with ' + JSON.stringify(options));
+  navigator.bluetooth.requestDevice(options)
+  .then(device => {
+    log('> Name:             ' + device.name);
+    log('> Id:               ' + device.id);
+    log('> Connected:        ' + device.gatt.connected);
+  })
+  .catch(error => {
+    log('Argh! ' + error);
+  });
+}
+
+/* Utils */
+
+function populateFiltersById(id) {
+  let filters = [];
+  
+  let filterService = document.querySelector(`#${id} .service`).value;
+  if (filterService.startsWith('0x')) {
+    filterService = parseInt(filterService);
+  }
+  if (filterService) {
+    filters.push({services: [filterService]});
+  }
+
+  let filterName = document.querySelector(`#${id} .name`).value;
+  if (filterName) {
+    filters.push({name: filterName});
+  }
+
+  let filterNamePrefix = document.querySelector(`#${id} .namePrefix`).value;
+  if (filterNamePrefix) {
+    filters.push({namePrefix: filterNamePrefix});
+  }
+
+  return filters;
+}
