1a428f9bdb9f21fa16e5a0d9952aa75a24070345
Thomas Steiner <tomac@google.com>
2025-10-02 19:59:06 +0200

    Remove demos

--- main-pwa-origin/script.js
+++
@@ -1,6 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', async () => {
-    const registration = await navigator.serviceWorker.register('sw.js');
-    console.log('Service worker registered for scope', registration.scope);
-  });
-}
