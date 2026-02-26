3fed973a272569d7c558c0496a595a561a8efd3c
Ishika Patel <ishikapatel@microsoft.com>
2026-02-26 15:32:36 -0800

    Adding test site for getInstalledRelatedApps API on Desktop PWAs
    Adding test site for getInstalledRelatedApps API on Desktop PWAs

---
+++ pwa-testing/gira-test-app/install.js
@@ -0,0 +1,26 @@
+/* Custom Install */
+let deferredPrompt;
+const btnAdd = document.getElementById('but-install');
+const installContainer = document.getElementById('install');
+
+// Handle install available
+window.addEventListener('beforeinstallprompt', (e) => {
+  showInstallPromo(e);
+});
+
+// Handle user request to install
+btnAdd.addEventListener('click', (e) => {
+  deferredPrompt.prompt();
+});
+
+// Hide the install button after app is installed
+window.addEventListener('appinstalled', (evt) => {
+  installContainer.classList.toggle('hidden', true);
+  deferredPrompt = null;
+});
+
+// Show the install button when install is available
+function showInstallPromo(e) {
+  deferredPrompt = e;
+  installContainer.classList.toggle('hidden', false);
+}
