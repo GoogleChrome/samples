73bd43862c52f062143ed5edd6b27864f3281285
Thomas Steiner <tomac@google.com>
2026-03-18 23:15:48 +0100

    Move to `dist/`

--- trailblazers/eleventy.config.js
+++ trailblazers/eleventy.config.js
@@ -265,7 +265,7 @@ export const config = {
     input: 'content', // default: "."
     includes: '../_includes', // default: "_includes" (`input` relative)
     data: '../_data', // default: "_data" (`input` relative)
-    output: '_site',
+    output: 'dist',
   },
 
   // -----------------------------------------------------------------
@@ -279,5 +279,5 @@ export const config = {
   // it will transform any absolute URLs in your HTML to include this
   // folder name and does **not** affect where things go in the output folder.
 
-  pathPrefix: '/samples/trailblazers/_site/',
+  pathPrefix: '/samples/trailblazers/dist/',
 };
