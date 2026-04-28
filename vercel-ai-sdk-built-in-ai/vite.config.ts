c6e283297f39aa91dd5139f131288a6bccd319f0
Thomas Steiner <tomac@google.com>
2026-04-28 19:50:20 +0200

    Fix for GH pages

--- vercel-ai-sdk-built-in-ai/vite.config.ts
+++ vercel-ai-sdk-built-in-ai/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/samples/vercel-ai-sdk-built-in-ai/dist/',
   plugins: [react()],
   resolve: {
     alias: {
